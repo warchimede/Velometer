4f4fa282b25cd0caa1162a67509a05f2d38fd812
William Archimede <william.archimede@hoodbrains.com>
2018-07-04 17:46:19 +0200

    Initial Commit

---
+++ Velometer/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Velometer
+//
+//  Created by William Archimède on 04/07/2018.
+//  Copyright © 2018 William Archimede. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+  override func viewDidLoad() {
+    super.viewDidLoad()
+    // Do any additional setup after loading the view, typically from a nib.
+  }
+
+  override func didReceiveMemoryWarning() {
+    super.didReceiveMemoryWarning()
+    // Dispose of any resources that can be recreated.
+  }
+
+
+}
+
---
+++ VelometerTests/VelometerTests.swift
@@ -0,0 +1,36 @@
+//
+//  VelometerTests.swift
+//  VelometerTests
+//
+//  Created by William Archimède on 04/07/2018.
+//  Copyright © 2018 William Archimede. All rights reserved.
+//
+
+import XCTest
+@testable import Velometer
+
+class VelometerTests: XCTestCase {
+    
+    override func setUp() {
+        super.setUp()
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+    
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+        super.tearDown()
+    }
+    
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+    
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+    
+}
