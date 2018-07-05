1ebf766a86bb538ce8f489faabc2625d7759a508
William Archimede <william.archimede@hoodbrains.com>
2018-07-05 18:27:59 +0200

    Current progress

---
+++ Velometer/Location/LocationManager.swift
@@ -0,0 +1,15 @@
+//
+//  LocationManager.swift
+//  Velometer
+//
+//  Created by William Archimède on 05/07/2018.
+//  Copyright © 2018 William Archimede. All rights reserved.
+//
+
+import CoreLocation
+
+class LocationManager {
+  static let shared = CLLocationManager()
+
+  private init() {}
+}
--- Velometer/ViewControllers/HomeViewController.swift
+++ Velometer/ViewControllers/HomeViewController.swift
@@ -7,6 +7,7 @@
 //
 
 import UIKit
+import CoreLocation
 
 class HomeViewController: UIViewController {
 
@@ -20,16 +21,28 @@ class HomeViewController: UIViewController {
   @IBOutlet weak var paceLabel: UILabel!
 
   private var race: Race?
+  private var seconds = 0
+  private var timer: Timer?
+  private var distance = Measurement(value: 0, unit: UnitLength.meters)
+  private var locationList: [CLLocation] = []
 
   override func viewDidLoad() {
     super.viewDidLoad()
     // Do any additional setup after loading the view, typically from a nib.
   }
 
+  override func viewWillDisappear(_ animated: Bool) {
+    super.viewWillDisappear(animated)
+    timer?.invalidate()
+    LocationManager.shared.stopUpdatingLocation()
+  }
+
   override func didReceiveMemoryWarning() {
     super.didReceiveMemoryWarning()
     // Dispose of any resources that can be recreated.
   }
+
+  // MARK: - Actions
 
   @IBAction func startRace(_ sender: Any) {
     startButton.isEnabled = false
@@ -42,5 +55,37 @@ class HomeViewController: UIViewController {
     stopButton.isEnabled = false
     historyButton.isEnabled = true
   }
+
+  // MARK: - Display
+
+  func eachSeconds() {
+    seconds += 1
+    updateDisplay()
+  }
+
+  private func updateDisplay() {
+    let formattedDistance = FormatDisplay.distance(distance)
+    let formattedTime = FormatDisplay.time(seconds)
+    let formattedPace = FormatDisplay.pace(distance: distance, seconds: seconds, outputUnit: UnitSpeed.minutesPerKilometer)
+
+    distanceLabel.text = formattedDistance
+    timeLabel.text = formattedTime
+    paceLabel.text = formattedPace
+  }
 }
 
+extension HomeViewController: CLLocationManagerDelegate {
+  func locationManager(_ manager: CLLocationManager, didUpdateLocations locations: [CLLocation]) {
+    for newLocation in locations {
+      let howRecent = newLocation.timestamp.timeIntervalSinceNow
+      guard newLocation.horizontalAccuracy < 20 && abs(howRecent) < 10 else { continue }
+
+      if let lastLocation = locationList.last {
+        let delta = newLocation.distance(from: lastLocation)
+        distance = distance + Measurement(value: delta, unit: UnitLength.meters)
+      }
+
+      locationList.append(newLocation)
+    }
+  }
+}
