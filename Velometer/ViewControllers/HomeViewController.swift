72b17bb12ff8dab87346b80ed981e66d62e851cd
William Archimede <william.archimede@hoodbrains.com>
2018-07-08 22:07:25 +0200

    Current progress

--- Velometer/ViewControllers/HomeViewController.swift
+++ Velometer/ViewControllers/HomeViewController.swift
@@ -7,6 +7,7 @@
 //
 
 import UIKit
+import MapKit
 import CoreLocation
 
 class HomeViewController: UIViewController {
@@ -20,26 +21,18 @@ class HomeViewController: UIViewController {
   @IBOutlet weak var speedLabel: UILabel!
   @IBOutlet weak var paceLabel: UILabel!
 
+  @IBOutlet weak var mapView: MKMapView!
+
   private var race: Race?
   private var seconds = 0
   private var timer: Timer?
   private var distance = Measurement(value: 0, unit: UnitLength.meters)
   private var locationList: [CLLocation] = []
 
-  override func viewDidLoad() {
-    super.viewDidLoad()
-    // Do any additional setup after loading the view, typically from a nib.
-  }
-
   override func viewWillDisappear(_ animated: Bool) {
     super.viewWillDisappear(animated)
     timer?.invalidate()
     LocationManager.shared.stopUpdatingLocation()
-  }
-
-  override func didReceiveMemoryWarning() {
-    super.didReceiveMemoryWarning()
-    // Dispose of any resources that can be recreated.
   }
 
   // MARK: - Actions
@@ -48,12 +41,30 @@ class HomeViewController: UIViewController {
     startButton.isEnabled = false
     stopButton.isEnabled = true
     historyButton.isEnabled = false
+
+    seconds = 0
+    distance = Measurement(value: 0, unit: UnitLength.meters)
+    locationList.removeAll()
+    updateDisplay()
+    timer = Timer.scheduledTimer(withTimeInterval: 1.0, repeats: true) { _ in
+      self.eachSeconds()
+    }
+    startLocationUpdates()
   }
 
   @IBAction func stopRace(_ sender: Any) {
     startButton.isEnabled = true
     stopButton.isEnabled = false
     historyButton.isEnabled = true
+
+    LocationManager.shared.stopUpdatingLocation()
+
+    let alert = UIAlertController(title: "The race is over", message: "Should it be saved ?", preferredStyle: .alert)
+    alert.addAction(UIAlertAction(title: "Save", style: .default) { _ in
+      self.saveRace()
+      self.loadMap()
+    })
+    present(alert, animated: true)
   }
 
   // MARK: - Display
@@ -71,10 +82,106 @@ class HomeViewController: UIViewController {
     distanceLabel.text = formattedDistance
     timeLabel.text = formattedTime
     paceLabel.text = formattedPace
+
+    if let coordinate = LocationManager.shared.location?.coordinate {
+      let delta = 0.005
+      let region = MKCoordinateRegion(center: coordinate, span: MKCoordinateSpan(latitudeDelta: delta, longitudeDelta: delta))
+      mapView.setRegion(region, animated: true)
+    }
+  }
+
+  // MARK: - Saving
+
+  private func saveRace() {
+    let newRace = Race(context: CoreDataStack.context)
+    newRace.distance = distance.value
+    newRace.duration = Int16(seconds)
+    newRace.date = Date()
+
+    locationList.forEach { location in
+      let locationObject = Location(context: CoreDataStack.context)
+      locationObject.timestamp = location.timestamp
+      locationObject.latitude = location.coordinate.latitude
+      locationObject.longitude = location.coordinate.longitude
+      newRace.addToLocations(locationObject)
+    }
+
+    CoreDataStack.saveContext()
+
+    race = newRace
   }
 }
 
+// MARK: - Mapping
+
+extension HomeViewController: MKMapViewDelegate {
+  private func mapRegion() -> MKCoordinateRegion? {
+    guard let locations = race?.locations, locations.count > 0
+      else {
+        return nil
+    }
+
+    let latitudes = locations.map { return ($0 as! Location).latitude }
+    let longitudes = locations.map { return ($0 as! Location).longitude }
+
+    let maxLat = latitudes.max()!
+    let minLat = latitudes.min()!
+    let maxLong = longitudes.max()!
+    let minLong = longitudes.min()!
+
+    let center = CLLocationCoordinate2D(latitude: (minLat + maxLat) / 2, longitude: (minLong + maxLong) / 2)
+    let span = MKCoordinateSpan(latitudeDelta: (maxLat - minLat) * 1.3, longitudeDelta: (maxLong - minLong) * 1.3)
+
+    return MKCoordinateRegion(center: center, span: span)
+  }
+
+  private func polyLine() -> MKPolyline {
+    guard let locations = race?.locations else {
+      return MKPolyline()
+    }
+
+    let coords: [CLLocationCoordinate2D] = locations.map {
+      let location = $0 as! Location
+      return CLLocationCoordinate2D(latitude: location.latitude, longitude: location.longitude)
+    }
+
+    return MKPolyline(coordinates: coords, count: coords.count)
+  }
+
+  private func loadMap() {
+    guard let locations = race?.locations,
+      locations.count > 0,
+      let region = mapRegion()
+      else {
+        return
+    }
+
+    mapView.setRegion(region, animated: true)
+    mapView.add(polyLine())
+  }
+
+  func mapView(_ mapView: MKMapView, rendererFor overlay: MKOverlay) -> MKOverlayRenderer {
+    guard let polyline = overlay as? MKPolyline else {
+      return MKOverlayRenderer(overlay: overlay)
+    }
+
+    let renderer = MKPolylineRenderer(polyline: polyline)
+    renderer.strokeColor = .blue
+    renderer.lineWidth = 3
+    return renderer
+  }
+}
+
+// MARK: - Location
+
 extension HomeViewController: CLLocationManagerDelegate {
+  private func startLocationUpdates() {
+    LocationManager.shared.delegate = self
+    LocationManager.shared.activityType = .fitness
+    LocationManager.shared.distanceFilter = 10
+    LocationManager.shared.startUpdatingLocation()
+  }
+
   func locationManager(_ manager: CLLocationManager, didUpdateLocations locations: [CLLocation]) {
     for newLocation in locations {
       let howRecent = newLocation.timestamp.timeIntervalSinceNow
